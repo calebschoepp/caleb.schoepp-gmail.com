2aa006a515172a286a4e9eb2d784b14c4141f747
Caleb <caleb.schoepp@gmail.com>
2020-03-28 17:55:51 -0700

    Intermediary stage for refactor of photo loading logic

--- src/Gallery.js
+++ src/Gallery.js
@@ -10,7 +10,7 @@ function Gallery() {
   // State
   const [category, setCategory] = useState("hot");
   const [posts, setPosts] = useState([]);
-  const [photos, setPhotos] = useState([]);
+  const [photos, setPhotos] = useState({}); // Map of postIdx to array of photos objects
   const [postIdx, setPostIdx] = useState(0);
   const [photoIdx, setPhotoIdx] = useState(0);
 
@@ -18,6 +18,7 @@ function Gallery() {
     // Everytime category is changed:
     // load posts for category from api
     // reset post and photo indexes
+    console.log("loading categories");
     getCategory(category).then(res => {
       setPosts(res.posts);
       setPostIdx(0);
@@ -30,22 +31,74 @@ function Gallery() {
     // load all of the photos for that post from api
     // reset photo index
     if (!posts[0]) {
+      // No posts loaded yet, don't do anything
       return;
     }
-    getPost(posts[postIdx]).then(res => {
-      setPhotos([res.original, ...res.photoshops]);
-      setPhotoIdx(0);
-    });
+    console.log("loading photos");
+
+    setPhotoIdx(0);
+
+    let promiseChain = Promise.resolve();
+    let loadIdx = postIdx - 1;
+    if (loadIdx >= 0 && !photos.hasOwnProperty(loadIdx)) {
+      console.log("start");
+      // Load post in top row if not already loaded
+      promiseChain = promiseChain.then(() => {
+        getPost(posts[loadIdx]).then(res => {
+          let setObj = {};
+          setObj[loadIdx] = [res.original, ...res.photoshops];
+          setPhotos(setObj);
+        });
+      });
+    }
+
+    loadIdx = postIdx;
+    if (!photos.hasOwnProperty(loadIdx)) {
+      console.log("middle");
+      // Load post in middle row if not already loaded
+      promiseChain = promiseChain.then(() => {
+        getPost(posts[loadIdx]).then(res => {
+          let setObj = {};
+          setObj[loadIdx] = [res.original, ...res.photoshops];
+          setPhotos(setObj);
+        });
+      });
+    }
+
+    loadIdx = postIdx + 1;
+    if (loadIdx < posts.length && !photos.hasOwnProperty(loadIdx)) {
+      console.log("end");
+      // Load post in bottom row if not already loaded
+      promiseChain = promiseChain.then(() => {
+        getPost(posts[loadIdx]).then(res => {
+          let setObj = {};
+          setObj[loadIdx] = [res.original, ...res.photoshops];
+          setPhotos(setObj);
+        });
+      });
+    }
+    console.log("promiseChain");
+    console.log(promiseChain);
+    return promiseChain;
   }, [posts, postIdx]);
+
+  console.log("------");
+  console.log("photos");
   console.log(photos);
+  // console.log("postIdx");
+  // console.log(postIdx);
+  // console.log("posts");
+  // console.log(posts);
   return (
     <>
       <CategoryPicker setCategory={setCategory} category={category} />
       <div>
         <PreviewImage
           display={postIdx > 0}
-          alt={"alt text here"}
-          url={"/test/1.jpg"}
+          alt={
+            postIdx > 0 && photos[postIdx - 1] && photos[postIdx - 1][0].text
+          }
+          url={postIdx > 0 && photos[postIdx - 1] && photos[postIdx - 1][0].url}
           onClick={() => setPostIdx(postIdx - 1)}
         />
         <div className="flex flex-row justify-center items-center">
@@ -75,9 +128,17 @@ function Gallery() {
           />
         </div>
         <PreviewImage
-          display={postIdx < posts.length}
-          alt={"alt text here"}
-          url={"/test/4.gif"}
+          display={postIdx < posts.length - 1}
+          alt={
+            postIdx < posts.length - 1 &&
+            photos[postIdx + 1] &&
+            photos[postIdx + 1][0].text
+          }
+          url={
+            postIdx < posts.length - 1 &&
+            photos[postIdx + 1] &&
+            photos[postIdx + 1][0].url
+          }
           onClick={() => setPostIdx(postIdx + 1)}
         />
       </div>
